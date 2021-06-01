f8eaceb2bec24d8d666b4fe8484c2d5b3aeb479c
Sixto Guerra <sixto.guerra1982@gmail.com>
2021-05-31 22:19:36 -0400

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+document.write("<div class='container row'><div class='col-6'><h1>Notas Finales</h1></div><img src='assets/img/logo.png' class='col-6 size-img'></div>")
+
+var nombre = prompt("Ingrese Nombre Alumno","Sixto Guerra")
+document.write("<div class='container'> Alumno: " + nombre + "</div>")
